d4cc6518b2f3006be82d82c97b6bebffc48883e2
marktwo <mamurray9999@gmail.com>
2016-05-15 15:55:31 +0930

    added exp4j library for evaluating mathematical expression

--- PiProject/src/main/java/pie/EstimatePi.java
+++ PiProject/src/main/java/pie/EstimatePi.java
@@ -13,6 +13,9 @@ package pie;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 /**
  * Produces an approximation to the numerical value of pi.
  * 
@@ -26,7 +29,7 @@ import java.util.Scanner;
  * @version 1.0
  */
 class EstimatePi {
-	
+
 	// Maximum number of terms to use in series
 	// Note: system limit is: 2^32 - 1
 	static int max_n = 0;
@@ -49,15 +52,15 @@ class EstimatePi {
 	/*
 	 * initialise this instance; get max_n
 	 */
-	private static void init(String[] args) {
+	private static void init(String[] userArgs) {
 		// only one argument required; max_n
-		if (args.length == 0 || args.length > 1) {
+		if (userArgs.length == 0 || userArgs.length > 1) {
 			System.out.println("Usage: estimatepi max_n\nWhere max_n is the maximum number of terms in the series.");
 			System.out.println("Example: estimatepi 100000");
 			System.exit(0);
 		} else {
 			try {
-				max_n = Integer.parseInt(args[0]);
+				max_n = Integer.parseInt(userArgs[0]);
 
 				if (max_n < 1) {
 					System.out.println("max_n must be a positive number, please try again.");
@@ -90,11 +93,11 @@ class EstimatePi {
 					System.out.println("n must be between 1 and " + max_n + ", please try again");
 				} else {
 					long tStart = System.currentTimeMillis();
-					
+
 					calculateSeriesTotal(nTerms);
 
 					long tDelta = System.currentTimeMillis() - tStart;
-					
+
 					pi = 4 * sum;
 					System.out.println("-> pi = " + pi);
 					System.out.println("-> t  = " + tDelta + " milliseconds");
@@ -109,9 +112,14 @@ class EstimatePi {
 
 	// calculate the sum of the series
 	private static void calculateSeriesTotal(int terms) {
-		// TODO Auto-generated method stub
+		// use exp4j for evaluating mathematical expression
+		// probably overkill for a simple expression
+		Expression ex = new ExpressionBuilder("(-1)^n / (2n + 1)").variables("n").build();
+		
 		for (int n = 0; n <= terms; n++) {
-			sum += ((Math.pow(-1, n)) / ((2 * n) + 1));
+			ex.setVariable("n", n);
+			sum += ex.evaluate();
 		}
+		
 	}
 }
